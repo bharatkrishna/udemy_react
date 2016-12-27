d7c90f7cb002eac057f983095d5563b6290811c9
Bharat Krishna <bharatkrishna.r@gmail.com>
2016-12-26 22:01:15 -0800

    Completed section 4

--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Book 2' },
-    { title: 'Book 3' },
-    { title: 'Book 4' },
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Book 2', pages: 30 },
+    { title: 'Book 3', pages: 87 },
+    { title: 'Book 4', pages: 1 },
   ];
 }
