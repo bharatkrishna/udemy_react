b0d773100e4fcaef18094560b75ab503942a2982
Bharat Krishna <bharatkrishna.r@gmail.com>
2016-12-26 21:50:52 -0800

    Add a book detail container. Upto mid of lecutre 49

--- book_list/src/containers/book-list.jsx
+++ book_list/src/containers/book-list.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          onClick={() => this.props.selectBook(book)}
+          key={book.title} className="list-group-item">{book.title}
+        </li>
       );
     });
   }
@@ -26,4 +31,14 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps) (BookList);
+// Anything returned form this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed
+  // to all of out reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps) (BookList);
